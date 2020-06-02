a91dd4fd03dd2c43226eaef623b0df164920bc71
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2020-06-02 22:35:30 +0300

    Add some deps (parser, macros lib)

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,21 +1,24 @@
-#[derive(Debug, PartialEq)]
+use strum_macros::EnumIter;
+use strum::IntoEnumIterator;
+
+#[derive(Clone, Debug, PartialEq, EnumIter)]
 pub enum Opcode {
-  HLT = 0,
-  LOAD = 1,
-  ADD = 2,
-  SUB = 3,
-  MUL = 4,
-  DIV = 5,
-  JMP = 6,
-  JMPF = 7,
-  JMPB = 8,
-  EQ = 9,
-  NEQ = 10,
-  GT = 11,
-  LT = 12,
-  JEQ = 13,
-  JNEQ = 14,
-  IGL = 255,
+  HLT = 0x0,
+  LOAD = 0x1,
+  ADD = 0x2,
+  SUB = 0x3,
+  MUL = 0x4,
+  DIV = 0x5,
+  JMP = 0x6,
+  JMPF = 0x7,
+  JMPB = 0x8,
+  EQ = 0x9,
+  NEQ = 0xA,
+  GT = 0xB,
+  LT = 0xC,
+  JEQ = 0xD,
+  JNEQ = 0xE,
+  IGL = 0xFF,
 }
 
 #[derive(Debug, PartialEq)]
@@ -40,21 +43,21 @@ impl From<Opcode> for u8 {
 impl From<u8> for Opcode {
   fn from(v: u8) -> Self {
       match v {
-        0 => Opcode::HLT,
-        1 => Opcode::LOAD,
-        2 => Opcode::ADD,
-        3 => Opcode::SUB,
-        4 => Opcode::MUL,
-        5 => Opcode::DIV,
-        6 => Opcode::JMP,
-        7 => Opcode::JMPF,
-        8 => Opcode::JMPB,
-        9 => Opcode::EQ,
-        10 => Opcode::NEQ,
-        11 => Opcode::GT,
-        12 => Opcode::LT,
-        13 => Opcode::JEQ,
-        14 => Opcode::JNEQ,
+        0x0 => Opcode::HLT,
+        0x1 => Opcode::LOAD,
+        0x2 => Opcode::ADD,
+        0x3 => Opcode::SUB,
+        0x4 => Opcode::MUL,
+        0x5 => Opcode::DIV,
+        0x6 => Opcode::JMP,
+        0x7 => Opcode::JMPF,
+        0x8 => Opcode::JMPB,
+        0x9 => Opcode::EQ,
+        0xA => Opcode::NEQ,
+        0xB => Opcode::GT,
+        0xC => Opcode::LT,
+        0xD => Opcode::JEQ,
+        0xE => Opcode::JNEQ,
         _ => Opcode::IGL,
       }
   }
@@ -67,9 +70,22 @@ mod tests {
   use super::*;
 
   #[test]
+  fn test_check_conversion() {
+      for opcode in Opcode::iter() {
+        let casted = Opcode::from(opcode.clone() as u8);
+        assert_eq!(casted, opcode);
+      }
+
+      // Some unknown values, make sure casted to IGL
+      assert_eq!(Opcode::from(244), Opcode::IGL);
+      assert_eq!(Opcode::from(254), Opcode::IGL);
+      assert_eq!(Opcode::from(253), Opcode::IGL);
+  }
+  
+  #[test]
   fn test_create_hlt() {
-      let opcode = Opcode::HLT;
-      assert_eq!(opcode, Opcode::HLT);
+      let instr = Instruction::new(Opcode::HLT);
+      assert_eq!(instr.opcode, Opcode::HLT);
   }
 
   #[test]
